89394f493eb0f3ce5736450bddd31dfb1437144f
Fatih Sarhan <f9n@protonmail.com>
2022-04-18 08:59:20 +0300

    Use lru_cache and Remove default values

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 import logging
+from functools import lru_cache
 
 from pydantic import BaseSettings
 
@@ -7,11 +8,10 @@ log = logging.getLogger("uvicorn")
 
 class Settings(BaseSettings):
     app_name: str = "Receipt Invoice OCR Rest API"
-    ocr_service_uri: str = (
-        "https://tesseract-4-all-langs-example.herokuapp.com/tesseract"
-    )
-    mongodb_uri: str = "mongodb+srv://root:0VnUxSMny1oOKMHR@impala.zrlzu.mongodb.net/receipt_and_invoice_db?retryWrites=true&w=majority"
+    ocr_service_uri: str
+    mongodb_uri: str
 
+@lru_cache()
 def get_settings() -> BaseSettings:
     log.info("Loading config settings from the environment...")
     return Settings()
