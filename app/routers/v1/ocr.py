12ac406f8b53a999dd860d0f1f6426b4d132436b
Fatih Sarhan <f9n@protonmail.com>
2022-04-22 23:03:29 +0300

    Add invoice ocr routes

--- app/routers/v1/ocr.py
+++ app/routers/v1/ocr.py
@@ -3,11 +3,21 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.post("/ocr/receipt")
+@router.post("/receipt")
 async def ocr_from_receipt():
     return {"message": "Get image and Send image_id field to you"}
 
 
-@router.get("/ocr/receipt/{image_id}")
+@router.get("/receipt/{image_id}")
 async def get_receipt_ocr_result():
     return {"message": "Send receipt ocr result"}
+
+
+@router.post("/invoice")
+async def ocr_from_invoice():
+    return {"message": "Get image and Send image_id field to you"}
+
+
+@router.get("/invoice/{image_id}")
+async def get_invoice_ocr_result():
+    return {"message": "Send invoice ocr result"}
