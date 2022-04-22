2e7dbe5b64f0ffb5b08088369f3b90af0d34b23a
Fatih Sarhan <f9n@protonmail.com>
2022-04-22 22:54:14 +0300

    Update receipt routes with new models

--- app/routers/v1/receipt.py
+++ app/routers/v1/receipt.py
@@ -1,93 +1,45 @@
-from fastapi import APIRouter, HTTPException
+from datetime import datetime
 
-from app.models import Receipt
+from fastapi import APIRouter, Depends, HTTPException, Body
+from beanie import PydanticObjectId
+
+from app.models import ReceiptIn, ReceiptDB
 
 router = APIRouter()
 
-dummy_receipts: list[Receipt] = [
-    {
-        "id": "6102",
-        "receipt_image_id": "61",
-        "receipt_created_date": "24/01/2022",
-        "firm": "Ozocr A.S.",
-        "no": "0023",
-        "date": "13/11/2021",
-        "total_amount": "81,90",
-        "total_kdv": "12,49",
-        "products": [
-            {
-                "name": "Cay",
-                "ratio_kdv": 8,
-                "quantity": 2,
-                "unit_price": "4,5",
-                "category": "icecek",
-            },
-            {
-                "name": "Makarna",
-                "ratio_kdv": 18,
-                "quantity": 3,
-                "unit_price": "3",
-                "category": "yiyecek",
-            },
-        ],
-    },
-    {
-        "id": "6103",
-        "receipt_image_id": "62",
-        "receipt_created_date": "25/01/2022",
-        "firm": "Kiskanc A.S.",
-        "no": "0023",
-        "date": "13/11/2021",
-        "total_amount": "81,90",
-        "total_kdv": "12,49",
-        "products": [
-            {
-                "name": "Kola",
-                "ratio_kdv": 8,
-                "quantity": 2,
-                "unit_price": "4,5",
-                "category": "icecek",
-            },
-            {
-                "name": "Et",
-                "ratio_kdv": 18,
-                "quantity": 3,
-                "unit_price": "3",
-                "category": "yiyecek",
-            },
-        ],
-    },
-]
+
+async def get_receipt(receipt_id: PydanticObjectId) -> ReceiptDB:
+    receipt = await ReceiptDB.get(receipt_id)
+    if receipt is None:
+        raise HTTPException(status_code=404, detail="Receipt not found")
+    return receipt
 
 
-@router.post("/")
-async def create_receipt():
-    return {"message": "Receipt created!"}
+@router.post("/", response_model=ReceiptDB)
+async def create_receipt(receipt: ReceiptIn):
+    receipt_created_date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    data = await ReceiptDB(
+        **receipt.dict(), receipt_created_date=receipt_created_date
+    ).create()
+    return data
 
 
-@router.get("/{receipt_id}", response_model=Receipt)
-async def get_receipt(receipt_id: str):
-    _receipt = None
-    for dummy_receipt in dummy_receipts:
-        if dummy_receipt["id"] == receipt_id:
-            _receipt = dummy_receipt
-
-    if _receipt is None:
-        raise HTTPException(status_code=404, detail="Receipt with given id not found")
-
-    return _receipt
+@router.get("/{receipt_id}", response_model=ReceiptDB)
+async def get_receipt(receipt: ReceiptDB = Depends(get_receipt)):
+    return receipt
 
 
 @router.put("/{receipt_id}")
-async def update_receipt(receipt_id: str):
-    return {"message": "Receipt updated!"}
+async def update_receipt(receipt: ReceiptDB = Depends(get_receipt)):
+    return {"message": "@TODO: Receipt updated!"}
 
 
 @router.delete("/{receipt_id}")
-async def delete_receipt(receipt_id: str):
+async def delete_receipt(receipt: ReceiptDB = Depends(get_receipt)):
+    await receipt.delete()
     return {"message": "Receipt deleted!"}
 
 
-@router.get("/", response_model=list[Receipt])
+@router.get("/", response_model=list[ReceiptDB])
 async def get_all_receipts():
-    return dummy_receipts
+    return await ReceiptDB.find_all().to_list()
---
+++ app/routers/v1/receipt_test.py
@@ -0,0 +1,56 @@
+"""
+dummy_receipts: list[Receipt] = [
+    {
+        "id": "6102",
+        "receipt_image_id": "61",
+        "receipt_created_date": "24/01/2022",
+        "firm": "Ozocr A.S.",
+        "no": "0023",
+        "date": "13/11/2021",
+        "total_amount": "81,90",
+        "total_kdv": "12,49",
+        "products": [
+            {
+                "name": "Cay",
+                "ratio_kdv": 8,
+                "quantity": 2,
+                "unit_price": "4,5",
+                "category": "icecek",
+            },
+            {
+                "name": "Makarna",
+                "ratio_kdv": 18,
+                "quantity": 3,
+                "unit_price": "3",
+                "category": "yiyecek",
+            },
+        ],
+    },
+    {
+        "id": "6103",
+        "receipt_image_id": "62",
+        "receipt_created_date": "25/01/2022",
+        "firm": "Kiskanc A.S.",
+        "no": "0023",
+        "date": "13/11/2021",
+        "total_amount": "81,90",
+        "total_kdv": "12,49",
+        "products": [
+            {
+                "name": "Kola",
+                "ratio_kdv": 8,
+                "quantity": 2,
+                "unit_price": "4,5",
+                "category": "icecek",
+            },
+            {
+                "name": "Et",
+                "ratio_kdv": 18,
+                "quantity": 3,
+                "unit_price": "3",
+                "category": "yiyecek",
+            },
+        ],
+    },
+]
+"""
